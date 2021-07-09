88e1c51b6ba4523ab9b09d64f6dea9d1a4e6396d
Kim Beom Jin <planet1995@nate.com>
2021-07-09 10:17:11 +0900

    [add] Link 컴포넌트를 사용하여 다른 주소로 이동하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // Route 컴포넌트를 사용하면 어떤 규칙을 가진 경로에 어떤 컴포넌트를 보여줄 지 정의
-import { Route } from 'react-router-dom'; 
+import { Route, Link } from 'react-router-dom'; 
 import Home from './Home';
 import About from './About';
 
@@ -9,6 +9,15 @@ function App() {
     // <Route path="주소규칙" component={보여줄 컴포넌트} />
     // exact: /(루트) 경로가 동일하므로 이를 방지하기 위한 설정
     <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
       <Route path="/" component={Home} exact /> 
       <Route path="/about" component={About} />
     </div>
